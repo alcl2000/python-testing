5521c001b36789894cc750691ae900b0bf781e06
alcl2000 <amy.555.lewis@gmail.com>
2023-01-19 14:51:21 +0000

    add: evens experiments

--- evens/evens.py
+++ evens/evens.py
@@ -8,17 +8,12 @@ def even_number_of_evens(numbers):
     if number of even numbers is even return true
     """
     if isinstance(numbers, list):
-        if numbers == []:
-            return False
-        else:
-            evens = 0 
-            for n in numbers:
-                if n % 2 == 0:
-                    evens += 1
-        return evens % 2 == 0
+        evens = (sum([1 for n in numbers if n % 2 == 0]))
+        return True if evens and evens % 2 == 0 else False
+        
     else:
         raise TypeError("A list was not being passed into the function")
 
 
 if __name__ == "__main__":
-    print(even_number_of_evens(5))
+    print(even_number_of_evens([1, 2, 4]))
